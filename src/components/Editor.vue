2607b117458b5db3e45d85a03f9ca3d9d4929422
Smith Orakpo <grapevinecode@gmail.com>
2019-11-24 14:42:14 -0800

    Set up editor and preview components

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>Editor</div>
+  <div>
+    <div class="center">EDITOR</div>
+    <div class="input-field">
+      <input type="text" v-model="note.title" id="title" />
+    </div>
+    <div class="input-field">
+      <textarea v-model="note.content" id="content" class="materialize-textarea"></textarea>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "editor"
+  name: "editor",
+  props: ["note"]
 };
 </script>
--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>Preview</div>
+  <div>
+    <div class="center">PREVIEW</div>
+    <div v-html="preview"></div>
+  </div>
 </template>
 
 <script>
+import marked from "marked";
+
 export default {
-  name: "preview"
+  name: "preview",
+  props: ["note"],
+  computed: {
+    preview() {
+      return marked(this.note.content);
+    }
+  }
 };
 </script>
