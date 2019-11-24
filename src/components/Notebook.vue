2ffae72443baec7ae8a2bf25f14ef553a68ec688
Smith Orakpo <grapevinecode@gmail.com>
2019-11-24 14:36:13 -0800

    Set up notebook and notes components

--- src/components/Notebook.vue
+++ src/components/Notebook.vue
@@ -12,26 +12,71 @@
     <div class="section">
       <div class="row">
         <div class="col s4">
-          <Notes />
+          <Notes :notes="notes" />
         </div>
-        <div class="col s4">
-          <Editor />
-        </div>
-        <div class="col s4">
-          <Preview />
-        </div>
+        <template v-if="note">
+          <div class="col s4">
+            <Editor :note="note" />
+          </div>
+          <div class="col s4">
+            <Preview :note="note" />
+          </div>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { event } from "@/main";
 import Notes from "./Notes";
 import Editor from "./Editor";
 import Preview from "./Preview";
 
 export default {
   name: "notebook",
+  data: () => ({
+    notes: undefined,
+    selectedID: undefined
+  }),
+  created() {
+    this.notes = JSON.parse(localStorage.getItem("notes")) || [];
+    this.selectedID = localStorage.getItem("selectedID") || null;
+    event.$on("create", note => this.create(note));
+    event.$on("select", id => this.select(id));
+    event.$on("remove", note => this.remove(note));
+  },
+  methods: {
+    create(note) {
+      this.notes.unshift(note);
+    },
+    select(id) {
+      this.selectedID = id;
+    },
+    remove(note) {
+      const index = this.notes.indexOf(note);
+      if (index !== -1) {
+        this.notes.splice(index, 1);
+      }
+    },
+    save() {
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+    }
+  },
+  computed: {
+    note() {
+      return this.notes.find(note => note.id === this.selectedID);
+    }
+  },
+  watch: {
+    notes: {
+      handler: "save",
+      deep: true
+    },
+    selectedID(val) {
+      localStorage.setItem("selectedID", val);
+    }
+  },
   components: {
     Notes,
     Editor,
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>Notes</div>
+  <div class="center">
+    <div>NOTES</div>
+    <div>
+      <button class="btn blue darken-2 white-text" @click.prevent="create">create</button>
+    </div>
+    <div class="collection" v-for="note in notes" :key="note.id">
+      <span class="center" @click.prevent="select(note.id)">{{ note.title }}</span>
+      <button class="btn-small red white-text right" @click.prevent="remove(note)">delete</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { event } from "@/main";
+import uuid from "uuid";
+
 export default {
-  name: "notes"
+  name: "notes",
+  props: ["notes"],
+  methods: {
+    create() {
+      const note = {
+        id: uuid.v4(),
+        title: "note title",
+        content: "note content"
+      };
+      event.$emit("create", note);
+    },
+    select(id) {
+      event.$emit("select", id);
+    },
+    remove(note) {
+      event.$emit("remove", note);
+    }
+  }
 };
 </script>
